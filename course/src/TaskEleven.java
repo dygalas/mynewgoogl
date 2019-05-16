2839e0646af4a513ed7abf03e88e856011a3315f
IKorkmazov <ibragim09_94@bk.ru>
2019-05-16 13:07:22 +0300

    Add 11th Task

---
+++ course/src/TaskEleven.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class TaskEleven {
+    public static void main(String[] args) {
+        System.out.println("Введите строку...");
+        Scanner scanner = new Scanner(System.in);
+        String in = scanner.nextLine();
+        System.out.println("Введенная строка: \n" + in);
+        }
+ }
