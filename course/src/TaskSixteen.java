6cfbca90e4794688f627abdb6a3943d3078a70dd
IKorkmazov <ibragim09_94@bk.ru>
2019-05-27 17:30:51 +0300

    16 Task fixed

--- course/src/TaskSixteen.java
+++ course/src/TaskSixteen.java
@@ -3,17 +3,16 @@ import java.io.*;
 
 public class TaskSixteen {
     public static void main(String[] args) {
-        try(BufferedReader br =
-       new BufferedReader(new FileReader(TaskSixteen.class.getResource("TaskSixteen.txt").getPath()))) {
-           String s;
+        try (BufferedReader br = new BufferedReader(new FileReader("TaskSixteen.txt"))) {
+            String s;
 
-           while ((s = br.readLine()) != null){
-               System.out.println(s);
-           }
-       } catch (FileNotFoundException e) {
-           e.printStackTrace();
-       } catch (IOException e) {
-           e.printStackTrace();
-       }
-     }
+            while ((s = br.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
